f74099780295f7ce90004f600d4c588ad4149576
Gloria Lim <glorialimartt@gmail.com>
2024-02-12 23:43:40 -0500

    Initial component structure

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './styles/Application.scss'
+// import './styles/Application.scss'
 
 const Application = () => {
 
---
+++ src/components/TopNavigationBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TopNavigationBar = () => {
+  
+}
+
+export default TopNavigationBar;
