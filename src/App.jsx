f1ac862566d55a8bc69bc3111b77dcc366ad3ab1
Gloria Lim <glorialimartt@gmail.com>
2024-02-15 18:00:35 -0500

    Add context and darkMode theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css';
 
-import TopNavigationBar from './components/TopNavigationBar';
 import NewMemo from './components/NewMemo';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Application from './components/Application';
+import { useAppContext } from './contexts/AppContext';
+
+
 function App() {
+  const { state } = useAppContext();
+  const darkMode = state.darkMode;
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light'
+    }
+  });
+  
   return (
-    <div className="App">
-      <TopNavigationBar />
-      <NewMemo />
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Application />
+        <NewMemo />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from  "react";
 // import './styles/Application.scss'
+import TopNavigationBar from "./TopNavigationBar";
 
 const Application = () => {
-
+  const { darkMode, setDarkMode } = useState();
+  
+  return (
+    <TopNavigationBar />
+  )
 }
 
 export default Application;
