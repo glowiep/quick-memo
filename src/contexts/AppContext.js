440a7c1bf8b27df2db60c01339f27374579eac08
Gloria Lim <glorialimartt@gmail.com>
2024-02-15 17:58:33 -0500

    Add context file

---
+++ src/contexts/AppContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const INITIAL_STATE = {
+  darkMode: false,
+  memoData: []
+}
+
+// Create context
+const AppContext = createContext();
+
+// Provider component to wrap app
+export const AppProvider = ({ children }) => {
+  // Entire state of the application
+  const [state, setState] = useState(INITIAL_STATE);
+
+  return (
+    <AppContext.Provider value={{ state, setState }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = ()=> {
+  return useContext(AppContext)
+}
