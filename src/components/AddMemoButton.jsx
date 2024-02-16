cc6ebb6d38161bd889faeac49d09bbf9518fd7fb
Gloria Lim <glorialimartt@gmail.com>
2024-02-15 19:20:46 -0500

    Rename component

---
+++ src/components/AddMemoButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/AddMemoButton.scss';
+import { Box, TextField  } from '@mui/material'
+
+const AddMemoButton = () => {
+  return (
+    <div className="add-memo-button">
+      <img 
+        className="memo__add-button" 
+        src="images/add.png"
+        alt="Add Memo"
+        onClick={() => console.log("Add new memo button clicked")}
+      />
+    </div>
+  )
+}
+
+export default AddMemoButton;
