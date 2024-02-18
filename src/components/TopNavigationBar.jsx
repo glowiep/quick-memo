ad30754cb82d8d0ff236e4b8f09a0c9b39837f47
Gloria Lim <glorialimartt@gmail.com>
2024-02-18 17:03:08 -0500

    Add export icon to navigation

--- src/components/TopNavigationBar.jsx
+++ src/components/TopNavigationBar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import '../styles/TopNavigationBar.scss';
 import ThemeToggle from "./ThemeToggle";
+import FileDownloadRoundedIcon from '@mui/icons-material/FileDownloadRounded';
+import IosShareRoundedIcon from '@mui/icons-material/IosShareRounded';
+import { Tooltip } from '@mui/material';
 
 const TopNavigationBar = () => {
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">✨ Quick Memo</span>
-      <ThemeToggle />
+      
+      <span className="top-nav-bar__logo">✨ Quick Memo </span>
+      <Tooltip title="Download memos as text file">
+        <IosShareRoundedIcon />
+      </Tooltip>
+      <Tooltip title="Toggle dark mode">
+        <ThemeToggle />
+      </Tooltip>
     </div>
   )
 }
