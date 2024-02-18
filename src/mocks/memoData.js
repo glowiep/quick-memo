4cf912c5d3a9151907fd4b0557de0ccd7e918bc2
Gloria Lim <glorialimartt@gmail.com>
2024-02-18 00:58:08 -0500

    Add to mock memoData

--- src/mocks/memoData.js
+++ src/mocks/memoData.js
@@ -1,7 +1,16 @@
 const memoData = [
   {
     memo: "This is memo 1 text."
-  }
+  },
+  {
+    memo: "This is memo 2 text."
+  },
+  {
+    memo: "This is memo 3 text."
+  },
+  {
+    memo: "This is memo 4 text."
+  },
 ];
 
 export default memoData;
