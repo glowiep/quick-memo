acad5d7a049e97bf559323adc1681b1ddb3d2e53
Gloria Lim <glorialimartt@gmail.com>
2024-02-17 19:12:05 -0500

    Remove unused component

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,15 +1,15 @@
-import React from  "react";
-import '../styles/Application.scss'
-import TopNavigationBar from "./TopNavigationBar";
-import MemoList from "./MemoList";
+// import React from  "react";
+// import '../styles/Application.scss'
+// import TopNavigationBar from "./TopNavigationBar";
+// import MemoList from "./MemoList";
 
-const Application = () => {
-  return (
-    <main id="application">
-      <TopNavigationBar />
-      <MemoList />
-    </main>
-  )
-}
+// const Application = () => {
+//   return (
+//     <main id="application">
+//       <TopNavigationBar />
+//       <MemoList />
+//     </main>
+//   )
+// }
 
-export default Application;
+// export default Application;
