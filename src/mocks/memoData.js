ea66d2453e85dd4d2c38aeb55f4ac891306e6ba2
Gloria Lim <glorialimartt@gmail.com>
2024-02-19 11:23:50 -0500

    Modify mock memoData

--- src/mocks/memoData.js
+++ src/mocks/memoData.js
@@ -1,26 +1,34 @@
 const memoData = [
   {
+    id: 0,
     memo: "This is memo 1 text."
   },
   {
+    id: 1,
     memo: "This is memo 2 text."
   },
   {
+    id: 2,
     memo: "This is memo 3 text."
   },
   {
+    id: 3,
     memo: "This is memo 4 text."
   },
   {
+    id: 4,
     memo: "This is memo 5 text."
   },
   {
+    id: 5,
     memo: "This is memo 6 text."
   },
   {
+    id: 6,
     memo: "This is memo 7 text."
   },
   {
+    id: 
     memo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
 ];
