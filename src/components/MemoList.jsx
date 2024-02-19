05a5182d1b571330a08cdf6ffb8d8b7c0a63c8f3
Gloria Lim <glorialimartt@gmail.com>
2024-02-19 10:48:46 -0500

    Point MemoList to use memoData in state instead of mockData

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import MemoListItem from "./MemoListItem";
-
+import { useAppContext } from "../contexts/AppContext";
 import { Stack } from '@mui/material';
 
 // Styling
 import '../styles/MemoList.scss'
 
-import memoData from '../mocks/memoData.js';
-
 const MemoList = () => {
+  const { state } = useAppContext();
   return(
   <div className="memo-list">
     <Stack direction="row" justifyContent="center"  spacing={2} useFlexGap flexWrap="wrap">
-      {memoData.map((memo, index) => (
+      {state.memoData.map((memo, index) => (
         <MemoListItem key={index} memoItemData={memo.memo}/>
       ))}
     </Stack>
