4b9268f0b26d994603c1d7b639a89b52a2ba979b
Gloria Lim <glorialimartt@gmail.com>
2024-02-19 11:24:11 -0500

    Modify mock memoData

--- src/mocks/memoData.js
+++ src/mocks/memoData.js
@@ -28,7 +28,7 @@ const memoData = [
     memo: "This is memo 7 text."
   },
   {
-    id: 
+    id: 7,
     memo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
 ];
