489652ccaa6d08f587840b0ddd068a215a8b0c75
Gloria Lim <glorialimartt@gmail.com>
2024-02-22 09:28:11 -0500

    Extract GithubIcon as component

---
+++ src/components/GithubIcon.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Tooltip } from '@mui/material'
+
+const GithubIcon = () => {
+  return (
+    <Tooltip title="View in Github">
+      <div id="visit-github" type="button" >
+        <a href="https://github.com/glowiep/quick-memo" target="_blank">
+          <GitHubIcon color="secondary" sx={{
+            height: "large"
+          }}/>
+        </a>
+      </div>
+    </Tooltip>
+  )
+}
+
+export default GithubIcon;
+
