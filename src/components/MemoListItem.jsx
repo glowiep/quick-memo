47f641e9ee736ae18d87e36275e6ff2e10fd2964
Gloria Lim <glorialimartt@gmail.com>
2024-03-10 03:38:55 -0400

    Create hook for useLocalStorage effect

--- src/components/MemoListItem.jsx
+++ src/components/MemoListItem.jsx
@@ -9,6 +9,7 @@ import useApplicationData from "../hooks/useApplicationData";
 import { ACTIONS, useAppContext } from "../contexts/AppContext";
 // Styling
 import '../styles/MemoListItem.scss';
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const MemoPaper = styled(Paper)(({ theme }) => ({
   width: 300,
@@ -23,18 +24,7 @@ const MemoListItem = (props) => {
   const { deleteMemo, copyMemo } = useApplicationData();
   const { state, dispatch } = useAppContext();
 
-  // Effect for handling memo data saved in local storage
-  useEffect(() => {
-    const localMemoData = window.localStorage.getItem('memoData');
-    if (localMemoData) {
-      const parsedMemoData = JSON.parse(localMemoData);
-      // Set memoData in state
-      dispatch({ type: ACTIONS.USE_LOCAL_STORAGE_MEMO_DATA, payload: parsedMemoData });
-    } else {
-      // If localMemoData is not defined, set the default value in local storage
-      window.localStorage.setItem('memoData', JSON.stringify(state.memoData));
-    }
-  }, [state.memoData, dispatch])
+  useLocalStorage();
 
   return (
     <div className="memo-item-div">
